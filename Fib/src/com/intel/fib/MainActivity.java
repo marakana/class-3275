39ebfbe6f69521e4a332b08a8abd286b770e295a
Marko Gargenta <marko@marakana.com>
2013-08-07 13:37:08 -0700

    Implemented basic event handling

--- Fib/src/com/intel/fib/MainActivity.java
+++ Fib/src/com/intel/fib/MainActivity.java
@@ -1,23 +1,37 @@
 package com.intel.fib;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+	private EditText input;
+	private Button buttonGo;
+	private TextView output;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main); // Inflate from XML to Java
 
+		input = (EditText) findViewById( R.id.input );
+		buttonGo = (Button) findViewById( R.id.button_go );
+		output = (TextView) findViewById( R.id.output );
+		
+		buttonGo.setOnClickListener( this );
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	public void onClick(View v) {
+		long n = Long.parseLong( input.getText().toString() );
+		
+		output.append( String.format("n=%d\n", n) );
+		
+	}
+
+	
 }
